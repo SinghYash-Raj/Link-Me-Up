4b0a86dd996f813d0afbe521e4ddbb659a86db2a
YashRajSingh <yash13raj.singh@gmail.com>
2023-09-20 21:41:17 +0530

    setting-up backend&frontend

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const ChatPage = () => {
-  const [chats, SetChats] = useState([]);
+  const [chats, setChats] = useState([]);
   const fetchChats = async () => {
     const { data } = await axios.get("/api/chat");
-    SetChats(data);
+    setChats(data);
   };
 
   useEffect(() => {
